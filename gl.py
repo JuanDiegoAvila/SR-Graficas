0200e297d2b0f83dada322840f936478581dc3a4
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2022-07-07 18:50:16 -0600

    Libreria interna

---
+++ gl.py
@@ -0,0 +1,23 @@
+def glInit():
+  print()
+
+def glCreateWindow():
+  print()
+
+def glViewPort(x, y, width, height):
+  print()
+
+def glClear():
+  print('clear')
+
+def glClearColor(r, g, b):
+  print()
+
+def glVertex(x, y):
+  print()
+
+def glColor(r, g, b):
+  print()
+
+def glFinish():
+  print()
