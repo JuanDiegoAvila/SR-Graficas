5188137f6a2dda070d27f2e05449d637754c0462
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2022-09-25 09:06:47 -0600

    feat: shader that draws saturn without rings

--- shaders.py
+++ shaders.py
@@ -14,26 +14,32 @@ def planeta(**kwargs):
 
     i = iA * w + iB * u + iC * v
     i *= -1
-    if y <= 800 and y > 750:
-        return color(clamping(103 * i), clamping(92 * i), clamping(77 * i))
-    if y <= 750 and y > 760:
-        return color(clamping(154 * i), clamping(139 * i), clamping(114 * i))
-    if y <= 700 and y > 620:
-        return color(clamping(190 * i), clamping(174 * i), clamping(147 * i))
-    if y <= 620 and y > 615:
-        return color(clamping(200 * i), clamping(180* i), clamping(148 * i))
-    if y <= 615 and y > 590:
-        return color(clamping(119 * i), clamping(106* i), clamping(87 * i))
-    if y <= 590 and y > 470:
-        return color(clamping(154 * i), clamping(139 * i), clamping(114 * i))
-    if y <= 470 and y > 430:
-        return color(clamping(119 * i), clamping(106* i), clamping(87 * i))
-    if y <= 430 and y > 400:
-        return color(clamping(200 * i), clamping(180* i), clamping(148 * i))
-    if y <= 400 and y > 380:
-        return color(clamping(103 * i), clamping(92 * i), clamping(77 * i))
-    else:
-        return color(clamping(95* i), clamping(85 * i), clamping(70 * i))
+
+    if y <= 900 and y > 890:
+        return color(clamping(133*i), clamping(112*i), clamping(93*i))
+    if y <= 890 and y > 875:
+        return color(clamping(150*i), clamping(130*i), clamping(105*i))
+    if y <= 875 and y > 840:
+        return color(clamping(156*i), clamping(139*i), clamping(100*i))
+    if y <= 840 and y > 730:
+        return color(clamping(156*i), clamping(139*i), clamping(100*i))
+    if y <= 730 and y > 700:
+        return color(clamping(170*i), clamping(128*i), clamping(100*i))
+    if y <= 700 and y > 650:
+        return color(clamping(191*i), clamping(173*i), clamping(143*i))
+    if y <= 650 and y > 620:
+        return color(clamping(189*i), clamping(169*i), clamping(135*i))
+    if y <= 620 and y > 500:
+        return color(clamping(197*i), clamping(176*i), clamping(142*i)) # bastante
+    if y <= 500 and y > 470:
+        return color(clamping(191*i), clamping(172*i), clamping(133*i))
+    if y <= 470 and y > 400:
+        return color(clamping(203*i), clamping(181*i), clamping(139*i))
+    if y <= 400 and y > 280:
+        return color(clamping(213*i), clamping(190*i), clamping(155*i)) # bastante
+    if y <= 280:
+        return color(clamping(159*i), clamping(139*i), clamping(113*i))
+
 
 
 def default(**kwargs):
